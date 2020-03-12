0f1a07d2722265ee0a99015329cd72b817789060
Iva-khar <ivakhar@gmail.com>
2020-03-12 14:17:36 +0200

    tutorial app ratu

--- web/docker_django/applications/data_converter/data_converter/urls.py
+++ web/docker_django/applications/data_converter/data_converter/urls.py
@@ -1,4 +1,4 @@
-"""data_convertor URL Configuration
+"""data_converter URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+# from django.conf.urls import url
+# from django.contrib import admin
+
+# urlpatterns = [
+#     url(r'^admin/', admin.site.urls),
+# ]
 from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path('ratu/', include('ratu.urls')),
+    path('admin/', admin.site.urls),
 ]
